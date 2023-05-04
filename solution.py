53155b941bda4c71b57cc36327cf1ca4269c1009
Aomine48 <130103880+Aomine48@users.noreply.github.com>
2023-05-04 20:50:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = (x.mean() - 0.5) / 3698.0
-    scale = 1 / (3698.0 * len(x))
-    return gamma.ppf(alpha / 2, len(x), loc=loc, scale=scale), \
-           gamma.ppf(1 - alpha / 2, len(x), loc=loc, scale=scale)
+    loc = (x.mean() + 0.5) / 3698.0
+    scale = np.sqrt(np.var(x)) / (3698.0 * np.sqrt(len(x)))
+    return loc - scale * norm.ppf(1 - alpha / 2), \
+           loc - scale * norm.ppf(alpha / 2)
+
